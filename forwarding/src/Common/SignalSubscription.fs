08fa8974045e949166867ac2f664ca1b4bd2dd86
Daniel Weller <daniel.weller@softwarepark.cc>
2023-08-23 15:40:50 +0200

    prepare for push subscription

--- forwarding/src/Common/SignalSubscription.fs
+++ forwarding/src/Common/SignalSubscription.fs
@@ -1,5 +1,34 @@
-﻿namespace Common
+﻿module Common.SignalSubscription
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System.Text.Json
+open System.Threading.Tasks
+open Aggregation.Contracts
+open Aggregation.Contracts.Signals.V1
+open FsToolkit.ErrorHandling
+open Giraffe
+open Microsoft.AspNetCore.Http
+
+type PubSubMessage = {
+    Data: string
+}
+
+type PubSubEvent = {
+    Message: PubSubMessage
+}
+
+let httpHandler (handler: Signal -> TaskResult<unit, exn>) = fun next (ctx: HttpContext) -> task {
+    let! body = ctx.ReadBodyFromRequestAsync()
+    let event = JsonSerializer.Deserialize<PubSubEvent>(body)
+    
+    let signal =
+        event.Message.Data
+        |> System.Convert.FromBase64String
+        |> System.Text.Encoding.UTF8.GetString
+        |> Signals.V1.deserialize
+    
+    let! result = signal |> handler
+    
+    match result with
+    | Ok _ -> return! json {|  |} next ctx
+    | Error e -> return! ServerErrors.INTERNAL_ERROR e.Message next ctx
+}
