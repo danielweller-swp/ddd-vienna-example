9d4047591a64f903469c7d000d001e1456a68952
Christian Folie <christian.folie@outlook.com>
2023-09-02 15:32:03 +0200

    terraform imports

--- aggregation/terraform/import.sh
+++ aggregation/terraform/import.sh
@@ -1,3 +1,3 @@
 #terraform import google_cloud_run_v2_service.aggregation-application europe-west1/aggregation-application
 #terraform import google_cloud_scheduler_job.aggregation-scheduler europe-west1/aggregation-schedule
-terraform import google_pubsub_topic.aggregation-signals projects/ddd-vienna-sample/topics/aggregation-signals
+#terraform import google_pubsub_topic.aggregation-signals projects/ddd-vienna-sample/topics/aggregation-signals
