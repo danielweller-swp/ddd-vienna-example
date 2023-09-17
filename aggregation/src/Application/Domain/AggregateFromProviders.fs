6071120ad03e3a6366e8eb1c5af7b5435233d401
Daniel Weller <daniel.weller@softwarepark.cc>
2023-09-17 10:30:24 +0200

    remove 'belong to mid' comments

--- aggregation/src/Application/Domain/AggregateFromProviders.fs
+++ aggregation/src/Application/Domain/AggregateFromProviders.fs
@@ -9,12 +9,8 @@ open Application.Domain.Providers
 open Application.Options
 open Microsoft.AspNetCore.Http
 open Aggregation.Contracts.Signals.V1
-open NodaTime
 
 open Giraffe
-
-// TODO: this belongs to early
-//let SIGNAL_TOPIC = "aggregation-signals" |> TopicIdentifier
 
 let publishSignal (bus: IBus) topic signal =
     signal
@@ -62,10 +58,7 @@ let aggregateAndPublishSignals bus topic (providers: IProvider list) = task {
 let handler = fun (next: HttpFunc) (ctx: HttpContext) -> task {
     let bus = ctx.GetService<IBus>()
     let providers = ctx.GetService<IProvider list>()
-    // TODO: this belongs to mid
     let config = ctx.GetService<ApplicationConfiguration>()
-    // TODO: this belongs to early
-    // do! aggregateAndPublishSignals clock bus SIGNAL_TOPIC providers
     do! aggregateAndPublishSignals bus config.SignalsTopicIdentifier providers
     return! json {|  |} next ctx
 }
