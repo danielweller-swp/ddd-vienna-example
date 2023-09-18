af2349fae5c6e079290aefacc515929609e0f1ac
Daniel Weller <daniel.weller@softwarepark.cc>
2023-09-18 09:49:30 +0200

    fix test

--- aggregation/tests/Contract.Tests/Signals.fs
+++ aggregation/tests/Contract.Tests/Signals.fs
@@ -1,7 +1,5 @@
 namespace Contract.Tests
 
-open System
-open Xunit
 open FsCheck.Xunit;
 open FsCheck;
 open Aggregation.Contracts;
@@ -10,6 +8,13 @@ type NodaTime =
     static member Instant() =
         Arb.Default.DateTimeOffset()
         |> Arb.convert NodaTime.Instant.FromDateTimeOffset (fun d -> d.ToDateTimeOffset())
+
+type NonEmptyString =
+    static member NonEmptyString() =
+        Arb.Default.NonEmptyString()
+        |> Arb.convert
+            (fun (nonEmptyString: FsCheck.NonEmptyString) -> nonEmptyString.Get)
+            FsCheck.NonEmptyString
 
 [<Properties( Arbitrary=[| typeof<NodaTime>; typeof<NonEmptyString> |] )>]
 module Signals =
